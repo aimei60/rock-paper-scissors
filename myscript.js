d159e016a1e35fd13a545d61853c269c28ce9593
aimei60 <104590763+aimei60@users.noreply.github.com>
2023-08-19 15:37:58 +0100

    Update the round of the game

--- myscript.js
+++ myscript.js
@@ -1,22 +1,30 @@
-function getComputerChoice() {
-    let computerchoice = ['Rock', 'Paper','Scissors'];
+let computerchoice = ['rock', 'paper','scissors'];
+function getComputerChoice(computerchoice) {
     return computerchoice[Math.floor(Math.random()*computerchoice.length)];
 }
 
+
 function playRound(playerselection, computerselection) {
+    console.log(playerselection, computerselection);
     if (playerselection === computerselection) {
         return "It's a tie!";
     }
     else if (playerselection === 'rock' && computerselection === 'scissors') {
         return "You win!";
     }
-
-
-
+    else if (playerselection === 'paper' && computerselection === 'rock') {
+        return "You win!";
+    }
+    else if (playerselection === 'scissors' && computerselection === 'paper') {
+        return "You win!";
+    }
+    else {
+        return "You Lose!";
+    }
 
 }
-let playerselection = 'paper'
-let computerselection = getComputerChoice()
-console.log(playRound(playerselection, computerselection))
+let playerselection = prompt("Choose your weapon").toLowerCase;
+let computerselection = getComputerChoice(computerchoice);
+console.log(playRound(playerselection,computerselection));
 
 
