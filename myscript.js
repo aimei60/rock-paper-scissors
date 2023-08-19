feb1ee6e35e45dbe1837a6d8d3d3707136693fd9
aimei60 <104590763+aimei60@users.noreply.github.com>
2023-08-19 06:58:55 +0100

    Defined the user and computer choice and enabled rock, paper and scissors to be picked randomly. I also printed out these to test if they worked.

--- myscript.js
+++ myscript.js
@@ -1,1 +1,7 @@
-console.log("hi")
+let userchoice = ['Rock', 'Paper','Scissor'];
+let computerchoice = ['Rock', 'Paper','Scissor'];
+
+playerselection = userchoice[Math.floor(Math.random()*userchoice.length)];
+computerselection = computerchoice[Math.floor(Math.random()*computerchoice.length)];
+console.log(playerselection)
+console.log(computerselection)
