38fdb4cca4853355dd0fad66a325ecb5a4877e5d
aimei60 <104590763+aimei60@users.noreply.github.com>
2023-08-20 14:01:28 +0100

    Adjusted player and computer scores and added a round function, as well as added to check the final scores

--- myscript.js
+++ myscript.js
@@ -1,45 +1,65 @@
-let computerchoice = ['rock', 'paper','scissors'];
+let playerscore = 0;
+let computerscore = 0;
+let rounds = 0;
+let totalrounds = 5;
+
 function getComputerChoice(computerchoice) {
+    computerchoice = ['rock', 'paper','scissors'];
     return computerchoice[Math.floor(Math.random()*computerchoice.length)];
 }
 
-let playerscore = 0
-let computerscore = 0
-
-function playRound (playerselection, computerselection){
+function playRound(playerselection, computerselection){
         console.log(playerselection, computerselection);
     
 if (playerselection === computerselection){
-    return "It's a tie!";
+    return "It's a tie! You both picked " + playerselection;
     }
 else if (playerselection === 'rock' && computerselection === 'scissors'){
-    playerscore = 1;
+    playerscore++;
     return "You win! " + playerselection + " " + "beat " + computerselection;
     }
 else if (playerselection === 'paper' && computerselection === 'rock'){
-    playerscore = 1;
+    playerscore++;
     return "You win! " + playerselection + " " + "beat " + computerselection;
     }
 else if (playerselection === 'scissors' && computerselection === 'paper'){
-    playerscore = 1;
+    playerscore++;
     return "You win! " + playerselection + " " + "beat " + computerselection;
     }
 
 else {
-    computerscore = 1;
+    computerscore++;
     return "You Lose! " + computerselection + " " + "beat " + playerselection;
     }
+    
+    rounds++
 
+}
+
+function game() {
+    while (rounds > totalrounds) {
+        const playerselection = prompt("Choose your weapon").toLowerCase();
+        if (playerselection === 'rock' || playerselection === 'paper' || playerselection === 'scissors') {
+            let computer = getComputerChoice();
+            console.log('Computer chose ' + computer);
+            playRound(playerselection, computer);
+        } else {
+            console.log('Invalid response. Please choose rock, paper or scissors.')
+        }
+    }
+}
+
+console.log("Game Over! Final scores are... You: " + playerscore + " " + "Computer: " + " " + computerscore);
+if (playerscore > computerscore) {
+    console.log("You win the Game! Congratulations!");
+} else if (computerscore > playerscore) {
+    console.log("Computer wins the Game! Sorry!")
+} else {
+    console.log("It's a tie!")
 }
 
 
 
-const playerselection = prompt("Choose your weapon").toLowerCase();
-const computerselection = getComputerChoice(computerchoice);
-
-console.log(playRound(playerselection,computerselection));
-console.log(computerscore)
-console.log(playerscore)
 
 
 
