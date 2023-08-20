97108eca7aa72c22cb5f02b5945ea74846195493
aimei60 <104590763+aimei60@users.noreply.github.com>
2023-08-20 08:09:05 +0100

    Added words to the you win/ lose string to highlight what the outcome is and added player and computer score

--- myscript.js
+++ myscript.js
@@ -3,6 +3,8 @@ function getComputerChoice(computerchoice) {
     return computerchoice[Math.floor(Math.random()*computerchoice.length)];
 }
 
+let playerscore = 0
+let computerscore = 0
 
 function playRound (playerselection, computerselection){
         console.log(playerselection, computerselection);
@@ -11,27 +13,33 @@ if (playerselection === computerselection){
     return "It's a tie!";
     }
 else if (playerselection === 'rock' && computerselection === 'scissors'){
-    return "You win!";
+    playerscore = 1;
+    return "You win! " + playerselection + " " + "beat " + computerselection;
     }
 else if (playerselection === 'paper' && computerselection === 'rock'){
-    return "You win!";
+    playerscore = 1;
+    return "You win! " + playerselection + " " + "beat " + computerselection;
     }
 else if (playerselection === 'scissors' && computerselection === 'paper'){
-    return "You win!";
+    playerscore = 1;
+    return "You win! " + playerselection + " " + "beat " + computerselection;
     }
 
 else {
-    return "You Lose!";
+    computerscore = 1;
+    return "You Lose! " + computerselection + " " + "beat " + playerselection;
     }
 
 }
+
 
 
 const playerselection = prompt("Choose your weapon").toLowerCase();
 const computerselection = getComputerChoice(computerchoice);
 
 console.log(playRound(playerselection,computerselection));
-
+console.log(computerscore)
+console.log(playerscore)
 
 
 
